93bd9938ac7c126d4bea3cec608ab96ca1ef9910
anakarolinaiclube-ux <anakarolina.iclube@gmail.com>
2025-12-01 20:56:31 -0300

    Delete backend.js

--- backend.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req,res){
-  try{
-    res.status(200).json({status:"ok", message:"Backend funcionando!"});
-  }catch(e){
-    res.status(500).json({error:e.toString()});
-  }
-}
